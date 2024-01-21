e578d96976121422b63c3929f34be5333412ed0e
minseo jang <mingdo.dev@gmail.com>
2024-01-22 01:46:49 +0900

    start spring boot project

---
+++ src/test/java/com/example/pharm/PharmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pharm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PharmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
