7469bda8bb42b3e65b99e8c285005acc3e8bd1e0
mingdodev <mingdo.dev@gmail.com>
2024-01-31 05:41:29 +0900

    feat: uriBuilder 서비스 구현

---
+++ src/main/java/com/example/pharm/api/service/KakaoUriBuilderService.java
@@ -0,0 +1,26 @@
+package com.example.pharm.api.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+
+@Slf4j
+@Service
+public class KakaoUriBuilderService {
+
+    private static final String KAKAO_LOCAL_SEARCH_ADDRESS_URL = "https://dapi.kakao.com/v2/local/search/address.json";
+
+    public URI buildUriByAddressSearch (String address) {
+
+        // URI를 만들 때 String을 붙여서 만들거나 스프링에서 제공해주는 컴포넌트를 사용한다.
+        UriComponentsBuilder uriBuilder = UriComponentsBuilder.fromHttpUrl(KAKAO_LOCAL_SEARCH_ADDRESS_URL);
+        uriBuilder.queryParam("query", address);
+
+        URI uri = uriBuilder.build().encode().toUri();
+        log.info("[KakaoUriBuilderService buildUriByAddressSearch] address: {}, uri: {}", address, uri);
+
+        return uri;
+    }
+}
