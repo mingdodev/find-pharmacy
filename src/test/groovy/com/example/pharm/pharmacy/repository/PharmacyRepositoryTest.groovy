438f95957c7cb11c3a0ec15eae3f8e5cd8f015a7
mingdodev <mingdo.dev@gmail.com>
2024-01-31 05:44:48 +0900

    test: PharmacyRepository save test

---
+++ src/test/groovy/com/example/pharm/pharmacy/repository/PharmacyRepositoryTest.groovy
@@ -0,0 +1,66 @@
+package com.example.pharm.pharmacy.repository
+
+import com.example.pharm.AbstractIntegrationContainerBaseTest
+import com.example.pharm.pharmacy.entity.Pharmacy
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PharmacyRepositoryTest extends AbstractIntegrationContainerBaseTest {
+
+    @Autowired
+    private PharmacyRepository pharmacyRepository
+
+    def setup() {
+        pharmacyRepository.deleteAll()
+    }
+
+    def "PharmacyRepository save"() {
+
+        given:
+        String address = "서울 특별시 성북구 종암동"
+        String name = "은혜 약국"
+        double latitude = 36.11
+        double longitude = 128.11
+
+        def pharmacy = Pharmacy.builder()
+                .pharmacyAddress(address)
+                .pharmacyName(name)
+                .latitude(latitude)
+                .longitude(longitude)
+                .build()
+
+        when:
+        def result = pharmacyRepository.save(pharmacy)
+
+        then:
+        result.getPharmacyAddress() == address
+        result.getPharmacyName() == name
+        result.getLatitude() == latitude
+        result.getLongitude() == longitude
+
+    }
+
+    def "PharmacyRepository saveAll"() {
+        given:
+        String address = "서울 특별시 성북구 종암동"
+        String name = "은혜 약국"
+        double latitude = 36.11
+        double longitude = 128.11
+
+        def pharmacy = Pharmacy.builder()
+                .pharmacyAddress(address)
+                .pharmacyName(name)
+                .latitude(latitude)
+                .longitude(longitude)
+                .build()
+
+        when:
+        pharmacyRepository.saveAll(Arrays.asList(pharmacy))
+        def result = pharmacyRepository.findAll()
+
+        then:
+        result.size() == 1
+    }
+
+}
